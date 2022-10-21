e43f067889b4d4fc18c98ea576cbf3f1980a5058
jirayu7991 <aosittichai7991@gmail.com>
2022-10-21 11:46:10 +0700

    .commit

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.header("Jirayu")
+st.subheader("Sittichai")
+
